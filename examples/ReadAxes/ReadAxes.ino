e1771e2a586c03c337e406f738fe89c337a59417
Ákos Szőts <szotsaki@gmail.com>
2018-04-10 09:05:28 +0200

    Update example

--- examples/ReadAxes/ReadAxes.ino
+++ examples/ReadAxes/ReadAxes.ino
@@ -1,7 +1,7 @@
 #include <I2C.h>
 #include <LIS331.h>
 
-#define ACCELEROMETER_ADDR 0x18
+#define ACCELEROMETER_ADDR 0x18 // Alternative: 0x19
 
 LIS331 accelerometer{ACCELEROMETER_ADDR};
 
@@ -69,7 +69,9 @@ void setup()
     I2c.setSpeed(1); // 400 kHz
     I2c.timeOut(2000); // Reset communication when it's blocking the system for more than 2 seconds
 
-    byte err = accelerometer.setPowerMode(LIS331::PowerMode::normalMode);
+    uint8_t err = accelerometer.setRebootMemoryContent(true); // Reset sensor register values to default
+    print_err(err);
+    err = accelerometer.setPowerMode(LIS331::PowerMode::normalMode);
     print_err(err);
     err = accelerometer.setDataRate(LIS331::DataRate::odr1000Hz);
     print_err(err);
@@ -87,7 +89,7 @@ void setup()
 
 void loop()
 {
-    int16_t x = 0, y = 0, z = 0;
+    int16_t x = 0, y = 0, z = 0; // Raw values. To get them in [g] use `getAxisValuesG()`
     accelerometer.getXValue(x);
     accelerometer.getYValue(y);
     accelerometer.getZValue(z);
